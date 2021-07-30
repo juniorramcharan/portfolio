12e402f34166a41407fe66df696239de095a0163
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-07-30 16:48:36 -0400

    completed contact section

--- js/site.js
+++ js/site.js
@@ -14,7 +14,27 @@ $(document).ready(function(){
     });
 
     //owl carousel script
-    $('.carousel').owlCarousel({
+    $('.carousel').owlCarousel({ 
+        margin:20,
+        nav:true,       
+        loop:true, 
+        autoplay: true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1,
+                nav: false
+            },
+            600:{
+                items:2,
+                nav: false
+            },
+            1000:{
+                items:3,
+                nav: false
+            }
+        }
 
     });
 });
